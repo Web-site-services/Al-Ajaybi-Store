b87b6141f020b8060946e02a1366e62e99042731
Web-site-services <matta.adolf@outlook.com>
2025-07-02 16:40:47 +0300

    Add files via upload

--- products.js
+++ products.js
@@ -4,16 +4,16 @@
 const products = [
   {
     id: "d1",
-    name: "دفتر سلك 60 ورقة",
-    price: 40,
+    name: "دفتر سلك 70 ورقة",
+    price: 90,
     mainCat: "أدوات مكتبية",
     category: "دفاتر",
-    stock: 70,
+    stock: 90,
     img: "https://i.imgur.com/Vp5j7dH.png"
   },
   {
     id: "d2",
-    name: "دفتر جامعي 700 ورقة",
+    name: "دفتر جامعي 100 ورقة",
     price: 13,
     mainCat: "أدوات مكتبية",
     category: "دفاتر",
@@ -36,6 +36,15 @@ const products = [
     mainCat: "مطبوعات",
     category: "بنس",
     stock: 50,
+    img: "https://i.imgur.com/zx9c59V.png"
+  },
+    {
+    id: "k10",
+    name: "كوع",
+    price: 90,
+    mainCat: "سباكه",
+    category: "كوع",
+    stock: 20,
     img: "https://i.imgur.com/zx9c59V.png"
   }
 ];
